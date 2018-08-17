bae1fd20b2dbabc8dd2b0365543ae0526fa511cc
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-08-17 10:22:46 +0200

    Added useKeyContainer (#9)

---
+++ examples/HTTPClientCertDownload.Net462.Classic/Program.cs
@@ -0,0 +1,40 @@
+﻿using OpenSSL.X509Certificate2Provider;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace HTTPClientCertDownload.Net462.Classic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // openssl req -newkey rsa:2048 -nodes -keyout key.pem -x509 -days 365 -out crt.pem
+            string certificateText = File.ReadAllText("crt.pem");
+            string privateKeyText = File.ReadAllText("key.pem");
+
+            ServicePointManager.Expect100Continue = true;
+            ServicePointManager.SecurityProtocol = (SecurityProtocolType)3072;
+            ServicePointManager.DefaultConnectionLimit = 9999;
+
+            // Ignore Server Certificate validation
+            ServicePointManager.ServerCertificateValidationCallback += (sender, cert, chain, sslPolicyErrors) => true;
+
+            HttpWebRequest req = (HttpWebRequest)WebRequest.Create(@"https://server.cryptomix.com/secure/");
+            req.PreAuthenticate = true;
+
+            ICertificateProvider provider = new CertificateFromFileProvider(certificateText, privateKeyText, true);
+            req.ClientCertificates.Add(provider.Certificate);
+
+            HttpWebResponse response = (HttpWebResponse)req.GetResponse();
+
+            var encoding = Encoding.ASCII;
+            using (var reader = new StreamReader(response.GetResponseStream(), encoding))
+            {
+                string responseText = reader.ReadToEnd();
+                Console.WriteLine(responseText);
+            }
+        }
+    }
+}
