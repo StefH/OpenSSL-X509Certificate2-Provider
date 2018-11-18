16f70b164ebdff4491d8a5c49899f7f1032f5529
Stef Heyenrath <Stef.Heyenrath@gmail.com>
2018-11-18 17:31:59 +0100

    Rename parameter in methods.

--- src/OpenSSL.PrivateKeyDecoder/OpenSSLPrivateKeyDecoder.cs
+++ src/OpenSSL.PrivateKeyDecoder/OpenSSLPrivateKeyDecoder.cs
@@ -30,14 +30,14 @@ namespace OpenSSL.PrivateKeyDecoder
         private const string PrivateEncryptedKeyFooter = "-----END ENCRYPTED PRIVATE KEY-----";
 
         /// <inheritdoc cref="IOpenSSLPrivateKeyDecoder.Decode"/>
-        public RSACryptoServiceProvider Decode(string privateText, SecureString password = null)
+        public RSACryptoServiceProvider Decode(string privateText, SecureString securePassword = null)
         {
             if (string.IsNullOrEmpty(privateText))
             {
                 throw new ArgumentNullException(nameof(privateText));
             }
 
-            var rsaParameters = DecodeParameters(privateText, password);
+            var rsaParameters = DecodeParameters(privateText, securePassword);
 
             // Create RSACryptoServiceProvider instance
             var rsaCryptoServiceProvider = new RSACryptoServiceProvider();
@@ -46,7 +46,7 @@ namespace OpenSSL.PrivateKeyDecoder
         }
 
         /// <inheritdoc cref="IOpenSSLPrivateKeyDecoder.DecodeParameters"/>
-        public RSAParameters DecodeParameters(string privateText, SecureString password = null)
+        public RSAParameters DecodeParameters(string privateText, SecureString securePassword = null)
         {
             if (string.IsNullOrEmpty(privateText))
             {
